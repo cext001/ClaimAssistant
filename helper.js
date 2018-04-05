9325c9978eba20b307c5972baa0abd8635cb7320
Harikrishnan V <39754@hexaware.com>
2018-04-05 11:28:35 +0530

    changes in helper

--- helper.js
+++ helper.js
@@ -23,7 +23,7 @@ module.exports = {
                 } else {
                     if(body.error){
                         console.log('Inside body error',body.error.message);
-                        speechOutput = ["'<s>"+body.error.message+"</s>'"];
+                        speechOutput = ['<s>'+body.error.message+'</s>'];
                     } else {
                         speechOutput = ["<s>According to our records, the current status of claim with ID <break strength=\"medium\" /> <say-as interpret-as='digits'> " + claimId + " </say-as>, is " + body.result.currentClaimStatus + ".</s>"];
                         if (body.result.currentClaimStatus === "On Hold") {
