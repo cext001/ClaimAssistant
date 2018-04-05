3c7c64f47ce7aa0dc41db143a15059175fa295b4
Harikrishnan V <39754@hexaware.com>
2018-04-05 12:53:31 +0530

    changes in server and helper

--- helper.js
+++ helper.js
@@ -2,6 +2,7 @@ const request = require('request');
 
 module.exports = {
     "getClaimStatus": function (claimId) {
+        var speechOutput =[];
         console.log('InsideHelper Claim Id:',claimId);
         //return new Promise(function (resolve, reject) {
             var options = {
@@ -12,10 +13,9 @@ module.exports = {
                 json: true
             };
             request(options, function (error, response, body) {
-                console.log('error:',error);
-                console.log('body:',body);
-                console.log('response:',response);
-                var speechOutput =[];
+                //console.log('error:',error);
+                //console.log('body:',body);
+                //console.log('response:',response);                
                 if (error) {
                     console.log(error);
                     speechOutput = ["<s>Something went wrong. Please try again</s>"];
@@ -26,7 +26,7 @@ module.exports = {
                         console.log('Inside body error',body.error.message);
                         speechOutput = ['<s>'+body.error.message+'</s>'];
                     } else {
-                        speechOutput = ["<s>According to our records, the current status of claim with ID <break strength=\"medium\" /> <say-as interpret-as=\"digits\"> 231233 </say-as>, is " + body.result.currentClaimStatus + ".</s>"];
+                        speechOutput = ["<s>According to our records, the current status of claim with ID <break strength=\"medium\" /> <say-as interpret-as=\"digits\"> "+claimId+"</say-as>, is " + body.result.currentClaimStatus + ".</s>"];
                         if (body.result.currentClaimStatus === "On Hold") {
                             speechOutput.push('<s>The reason for the same is <break strength=\"medium\" />' + body.result.reason + '.</s>');
                         }
