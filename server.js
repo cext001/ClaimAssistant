440e842bf72cda9ebb82809a09d564ea0f398916
Harikrishnan V <39754@hexaware.com>
2018-04-05 14:26:47 +0530

    changes in server and helper

--- server.js
+++ server.js
@@ -219,14 +219,14 @@ alexaApp.intent('claimIdIntent', function (request, response) {
         claimId = (claimId.replace(/(\d{3})(\d{2})(\d{6})/, "$1-$2-$3"));
         console.log('After change::',claimId);
         if(claimStatusIntentCalled){
-            helper.getClaimStatus(claimId,response){
+            helper.getClaimStatus(claimId,response);/*{
                 //say = result;
                 //say= ["<s> According to our records, the current status of claim with ID <break strength=\"medium\" /> <say-as interpret-as=\"digits\"> 231233 </say-as>, is On Hold.</s>"];
                 console.log('after call',say);
                 //response.shouldEndSession(false);
                 //response.say(say.join('\n'));                
 
-            }
+            }*/
         }
         if(repairPaymentIntentCalled){
             getRepairPaymentStatus(claimId,function(responseText){
